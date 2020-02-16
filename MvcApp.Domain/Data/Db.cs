51a6476389cc88117da0210fc5cdbb272579cf7b
A11Part6T0g37er <indieware@protonmail.com>
2020-02-16 23:14:25 +0200

    DomainApp

---
+++ MvcApp.Domain/Data/Db.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+
+namespace MvcApp.Models.Data
+{
+    public class Db : DbContext
+    {
+        static Db()
+        {
+            Database.SetInitializer<Db>(new ArticleInitializer());
+        }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<ProfDTO> Profs { get; set; }
+        public DbSet<Person> People { get; set; }
+
+    }
+
+}
